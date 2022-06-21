a68aa4276ae45a47774e3a8b54f3ae98610cb0a9
Tony Narlock <tony@git-pull.com>
2022-06-21 10:45:54 -0500

    Include TableColumnResize
    See also:
    - https://ckeditor.com/docs/ckeditor5/latest/features/table.html#table-column-resize
    - https://ckeditor.com/docs/ckeditor5/latest/api/module_table_tablecolumnresize-TableColumnResize.html

--- packages/ckeditor5-build-eduflow/src/ckeditor.js
+++ packages/ckeditor5-build-eduflow/src/ckeditor.js
@@ -33,6 +33,7 @@ import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
@@ -186,6 +187,7 @@ const plugins = [
 	Mention,
 	Strikethrough,
 	Table,
+	TableColumnResize,
 	TableToolbar,
 	TableProperties,
 	TableCellProperties,
