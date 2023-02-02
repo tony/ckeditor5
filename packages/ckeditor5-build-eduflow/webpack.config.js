0daed707fd1817837e99c9bf8f5631b56d6fd160
Tony Narlock <tony@git-pull.com>
2023-02-02 06:40:47 -0600

    webpack: Add typescript and update license header

--- packages/ckeditor5-build-eduflow/webpack.config.js
+++ packages/ckeditor5-build-eduflow/webpack.config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -226,8 +226,15 @@ module.exports = {
 						}
 					}
 				]
+			},
+			{
+				test: /\.ts$/,
+				use: [ 'ts-loader' ]
 			}
 		]
-	}
+	},
 
+	resolve: {
+		extensions: [ '.ts', '.js', '.json' ]
+	}
 };
