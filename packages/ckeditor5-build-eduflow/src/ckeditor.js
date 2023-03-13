51a56b7e51a9f0315360c64c2efd85be0693dac8
Tony Narlock <tony@git-pull.com>
2023-03-13 15:26:39 -0500

    Tag v36.0.1-next.4

--- packages/ckeditor5-build-eduflow/src/ckeditor.js
+++ packages/ckeditor5-build-eduflow/src/ckeditor.js
@@ -43,7 +43,7 @@ import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Mention from '@ckeditor/ckeditor5-mention/src/mention';
-import Mathematics from 'ckeditor5-math/src/math';
+import Mathematics from '@tony/ckeditor5-math/src/math';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
