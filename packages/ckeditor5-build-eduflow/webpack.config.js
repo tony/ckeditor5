f446c294de796937e621ebdc973beb7840c66d85
Tony Narlock <tony@git-pull.com>
2023-03-26 14:51:24 -0500

    Revert "Temporary: Inline source map"
    This reverts commit 74fc966c9c663c47ca08bb72bff364327d84c948.

--- packages/ckeditor5-build-eduflow/webpack.config.js
+++ packages/ckeditor5-build-eduflow/webpack.config.js
@@ -14,7 +14,7 @@ const { CKEditorTranslationsPlugin } = require( '@ckeditor/ckeditor5-dev-transla
 const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
-	devtool: 'inline-source-map',
+	devtool: 'source-map',
 	performance: { hints: false },
 
 	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
